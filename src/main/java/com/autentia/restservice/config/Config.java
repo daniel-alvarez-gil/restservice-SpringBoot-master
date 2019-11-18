b942eed2d6c4ef1522423103f74510756c9b1f37
David del Castillo <ddelcastillor.david@gmail.com>
2019-11-18 19:58:32 +0100

    Incluir endpoint securizado

--- src/main/java/com/autentia/restservice/config/Config.java
+++ src/main/java/com/autentia/restservice/config/Config.java
@@ -9,17 +9,23 @@ import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConve
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EnableSwagger2
 @Configuration
 @EnableSpringDataWebSupport
 public class Config implements WebMvcConfigurer {
-
+/*
         @Bean
         public Docket api()
         {
@@ -28,7 +34,33 @@ public class Config implements WebMvcConfigurer {
                     .apis(RequestHandlerSelectors.any())
                     .paths(PathSelectors.any())
                     .build();
-        }
+        }*/
+
+    @Bean
+    public Docket api() {
+
+        SecurityReference securityReference = SecurityReference.builder()
+                .reference("basicAuth")
+                .scopes(new AuthorizationScope[0])
+                .build();
+
+        ArrayList<SecurityReference> reference = new ArrayList<>(1);
+        reference.add(securityReference);
+
+        ArrayList<SecurityContext> securityContexts = new ArrayList<>(1);
+        securityContexts.add(SecurityContext.builder().securityReferences(reference).build());
+
+        ArrayList<SecurityScheme> auth = new ArrayList<>(1);
+        auth.add(new BasicAuth("basicAuth"));
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(auth)
+                .securityContexts(securityContexts)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 
     @Bean
     public MappingJackson2XmlHttpMessageConverter mappingJackson2XmlHttpMessageConverter() {
--- src/main/java/com/autentia/restservice/config/SecurityConfig.java
+++ src/main/java/com/autentia/restservice/config/SecurityConfig.java
@@ -1,52 +1,25 @@
-/*
 package com.autentia.restservice.config;
 
-import io.swagger.models.HttpMethod;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("admin").password("password").roles("USER", "ADMIN");
-
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/api/v1/songs/secure").authenticated().and().httpBasic();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                // .antMatchers("api/v1/songs/secure").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .httpBasic();
-
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}pass") // Spring Security 5 requires specifying the password storage format
+                .roles("USER");
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-    }
-}*/
+}
